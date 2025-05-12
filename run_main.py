62f08e9391940f1c33fdede311498ebfdecf90fb
rohitb0017 <rohitbasu8953@gmail.com>
2025-05-12 16:24:43 +0530

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -1,6 +1,6 @@
 import argparse
 import torch
-from accelerate import Accelerator, DeepSpeedPlugin
+from accelerate import Accelerator
 from accelerate import DistributedDataParallelKwargs
 from torch import nn, optim
 from torch.optim import lr_scheduler
@@ -27,81 +27,74 @@ torch.manual_seed(fix_seed)
 np.random.seed(fix_seed)
 
 # basic config
-parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast',
-                    help='task name, options:[long_term_forecast, short_term_forecast, imputation, classification, anomaly_detection]')
-parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
-parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
-parser.add_argument('--model_comment', type=str, required=True, default='none', help='prefix when saving test results')
-parser.add_argument('--model', type=str, required=True, default='Autoformer',
-                    help='model name, options: [Autoformer, DLinear]')
-parser.add_argument('--seed', type=int, default=2021, help='random seed')
+parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast')
+parser.add_argument('--is_training', type=int, required=True, default=1)
+parser.add_argument('--model_id', type=str, required=True, default='test')
+parser.add_argument('--model_comment', type=str, required=True, default='none')
+parser.add_argument('--model', type=str, required=True, default='Autoformer')
+parser.add_argument('--seed', type=int, default=2021)
 
 # data loader
-parser.add_argument('--data', type=str, required=True, default='ETTm1', help='dataset type')
-parser.add_argument('--root_path', type=str, default='./dataset', help='root path of the data file')
-parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
-parser.add_argument('--features', type=str, default='M',
-                    help='forecasting task, options:[M, S, MS]; '
-                         'M:multivariate predict multivariate, S: univariate predict univariate, '
-                         'MS:multivariate predict univariate')
-parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
-parser.add_argument('--loader', type=str, default='modal', help='dataset type')
-parser.add_argument('--freq', type=str, default='h',
-                    help='freq for time features encoding, '
-                         'options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], '
-                         'you can also use more detailed freq like 15min or 3h')
-parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+parser.add_argument('--data', type=str, required=True, default='ETTm1')
+parser.add_argument('--root_path', type=str, default='./Time-LLM/dataset', help='Modified for Kaggle path')
+parser.add_argument('--data_path', type=str, default='ETTh1.csv')
+parser.add_argument('--features', type=str, default='M')
+parser.add_argument('--target', type=str, default='OT')
+parser.add_argument('--loader', type=str, default='modal')
+parser.add_argument('--freq', type=str, default='h')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/')
 
 # forecasting task
-parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
-parser.add_argument('--label_len', type=int, default=48, help='start token length')
-parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
-parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
+parser.add_argument('--seq_len', type=int, default=96)
+parser.add_argument('--label_len', type=int, default=48)
+parser.add_argument('--pred_len', type=int, default=96)
+parser.add_argument('--seasonal_patterns', type=str, default='Monthly')
 
 # model define
-parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
-parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-parser.add_argument('--c_out', type=int, default=7, help='output size')
-parser.add_argument('--d_model', type=int, default=16, help='dimension of model')
-parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
-parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
-parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
-parser.add_argument('--d_ff', type=int, default=32, help='dimension of fcn')
-parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
-parser.add_argument('--factor', type=int, default=1, help='attn factor')
-parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
-parser.add_argument('--embed', type=str, default='timeF',
-                    help='time features encoding, options:[timeF, fixed, learned]')
-parser.add_argument('--activation', type=str, default='gelu', help='activation')
-parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
-parser.add_argument('--patch_len', type=int, default=16, help='patch length')
-parser.add_argument('--stride', type=int, default=8, help='stride')
-parser.add_argument('--prompt_domain', type=int, default=0, help='')
-parser.add_argument('--llm_model', type=str, default='LLAMA', help='LLM model') # LLAMA, GPT2, BERT
-parser.add_argument('--llm_dim', type=int, default='4096', help='LLM model dimension')# LLama7b:4096; GPT2-small:768; BERT-base:768
-
+parser.add_argument('--enc_in', type=int, default=7)
+parser.add_argument('--dec_in', type=int, default=7)
+parser.add_argument('--c_out', type=int, default=7)
+parser.add_argument('--d_model', type=int, default=16)
+parser.add_argument('--n_heads', type=int, default=8)
+parser.add_argument('--e_layers', type=int, default=2)
+parser.add_argument('--d_layers', type=int, default=1)
+parser.add_argument('--d_ff', type=int, default=32)
+parser.add_argument('--moving_avg', type=int, default=25)
+parser.add_argument('--factor', type=int, default=1)
+parser.add_argument('--dropout', type=float, default=0.1)
+parser.add_argument('--embed', type=str, default='timeF')
+parser.add_argument('--activation', type=str, default='gelu')
+parser.add_argument('--output_attention', action='store_true')
+parser.add_argument('--patch_len', type=int, default=16)
+parser.add_argument('--stride', type=int, default=8)
+parser.add_argument('--prompt_domain', type=int, default=0)
+parser.add_argument('--llm_model', type=str, default='LLAMA')
+parser.add_argument('--llm_dim', type=int, default=4096)
 
 # optimization
-parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
-parser.add_argument('--itr', type=int, default=1, help='experiments times')
-parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
-parser.add_argument('--align_epochs', type=int, default=10, help='alignment epochs')
-parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
-parser.add_argument('--eval_batch_size', type=int, default=8, help='batch size of model evaluation')
-parser.add_argument('--patience', type=int, default=10, help='early stopping patience')
-parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
-parser.add_argument('--des', type=str, default='test', help='exp description')
-parser.add_argument('--loss', type=str, default='MSE', help='loss function')
-parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
-parser.add_argument('--pct_start', type=float, default=0.2, help='pct_start')
-parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+parser.add_argument('--num_workers', type=int, default=10)
+parser.add_argument('--itr', type=int, default=1)
+parser.add_argument('--train_epochs', type=int, default=10)
+parser.add_argument('--align_epochs', type=int, default=10)
+parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--eval_batch_size', type=int, default=8)
+parser.add_argument('--patience', type=int, default=10)
+parser.add_argument('--learning_rate', type=float, default=0.0001)
+parser.add_argument('--des', type=str, default='test')
+parser.add_argument('--loss', type=str, default='MSE')
+parser.add_argument('--lradj', type=str, default='type1')
+parser.add_argument('--pct_start', type=float, default=0.2)
+parser.add_argument('--use_amp', action='store_true', default=False)
 parser.add_argument('--llm_layers', type=int, default=6)
 parser.add_argument('--percent', type=int, default=100)
 
 args = parser.parse_args()
+
+# Removed deepspeed_plugin and fixed path
 ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
-#deepspeed_plugin = DeepSpeedPlugin(hf_ds_config='./ds_config_zero2.json')
 accelerator = Accelerator(kwargs_handlers=[ddp_kwargs])
+
+# (rest of the script continues unchanged...)
 
 for ii in range(args.itr):
     # setting record of experiments
